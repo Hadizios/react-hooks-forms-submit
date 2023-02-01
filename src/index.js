b17bbb95139ef67e1c48e62646737434cffa5d35
khaldar <adnan-5-@outlook.com>
2023-02-01 19:09:11 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import React from "react";
+import ReactDOM from "react-dom";
+import Form from "./components/Form";
 
-ReactDOM.render(
-  <Form />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Form />, document.getElementById("root"));
